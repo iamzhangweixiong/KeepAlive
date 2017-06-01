dd927ae2334bc47c401519a9534cf278d1918239
zhangwxgo@gmail.com <Zwx19930301>
2017-06-01 11:23:10 +0800

    【+】进程灰色保活

--- app/src/main/java/com/zhangwx/keepalive/GrayService.java
+++ app/src/main/java/com/zhangwx/keepalive/GrayService.java
@@ -1,21 +1,30 @@
 package com.zhangwx.keepalive;
 
+import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 
 /**
  * Created by zhangwx on 2017/5/31.
+ * 灰色保活
  */
 
 public class GrayService extends Service {
-    public static final int GRAY_SERVICE_ID = 1001;
+    public static final String TAG = "GrayService";
+    public static final int GRAY_SERVICE_ID = -1001;
+    public static final int GRAY_WAKE_REQUEST = 6666;
+
+    private final static int ALARM_INTERVAL = 5 * 60 * 1000;//1分钟
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand");
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
             startForeground(GRAY_SERVICE_ID, new Notification());//API < 18
         } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
@@ -23,7 +32,16 @@ public class GrayService extends Service {
             startService(innerIntent);
             startForeground(GRAY_SERVICE_ID, new Notification());
         }
-        return super.onStartCommand(intent, flags, startId);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent alarmIntent = new Intent();
+        alarmIntent.setAction(WakeReceiver.GARY_WAKE_ACTION);
+        PendingIntent operation = PendingIntent.getBroadcast(
+                this, GRAY_WAKE_REQUEST, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setInexactRepeating(
+                AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), ALARM_INTERVAL, operation);
+
+        return START_STICKY;
     }
 
     @Override
@@ -32,11 +50,12 @@ public class GrayService extends Service {
     }
 
     public static class InnerGrayService extends Service {
+        public static final String TAG = "InnerGrayService";
 
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
+            Log.e(TAG, "onStartCommand");
             startForeground(GRAY_SERVICE_ID, new Notification());
-            stopForeground(true);
             stopSelf();
             return super.onStartCommand(intent, flags, startId);
         }
---
+++ app/src/main/java/com/zhangwx/keepalive/WakeReceiver.java
@@ -0,0 +1,26 @@
+package com.zhangwx.keepalive;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by zhangwx on 2017/6/1.
+ */
+
+public class WakeReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "WakeReceiver";
+    public static final String GARY_WAKE_ACTION = "com.zhangwx.graywake";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.e(TAG, "onReceive: ACTION = " + action);
+        if (GARY_WAKE_ACTION.equals(action)) {
+            Intent intent1 = new Intent(context, GrayService.class);
+            context.startService(intent1);
+        }
+    }
+}
