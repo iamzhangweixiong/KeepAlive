001757a1932f3bda84d9ed4a81924939aa8ed9d8
zhangwx <zhangwxgo@gmail.com>
2017-09-04 17:12:19 +0800

    【add】SchedulerJob

--- app/src/main/java/com/zhangwx/keepalive/jobscheduler/JobSchedulerService.java
+++ app/src/main/java/com/zhangwx/keepalive/jobscheduler/JobSchedulerService.java
@@ -3,7 +3,11 @@ package com.zhangwx.keepalive.jobscheduler;
 import android.annotation.TargetApi;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import com.zhangwx.keepalive.common.WeakHandler;
 
 /**
  * Created by zhangwx
@@ -11,14 +15,34 @@ import android.os.Build;
  */
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-public class JobSchedulerService extends JobService{
+public class JobSchedulerService extends JobService {
+
+    private WeakHandler mJobHandler = new WeakHandler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            // do something
+            jobFinished((JobParameters) msg.obj, false);
+            return true;
+        }
+    });
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        Message msg = Message.obtain();
+        msg.what = 1;
+        msg.obj = params;
+        mJobHandler.sendMessage(msg);
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        mJobHandler.removeCallbacksAndMessages(null);
         return false;
     }
 }
